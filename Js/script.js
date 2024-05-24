97f088ffe455a95c3d95fcfdab22894ecf1028d9
Araarasimpp <nicolasrios.078@gmail.com>
2024-05-24 12:51:56 -0500

    PUNTOS DE REF

--- Js/script.js
+++ Js/script.js
@@ -99,3 +99,62 @@ btnPopup.addEventListener("click", () => {
 closeIcon.addEventListener("click", () => {
   modal.classList.remove("activate-popup");
 });
+
+// Lista de puntos de referencia con sus coordenadas
+var points = [
+  { name: "DESPACHO LA MARIA", coords: [3.308608, -76.539735] },
+  { name: "CAÑAS GORDAS", coords: [3.324761, -76.534010] },
+  { name: "ICESI", coords: [3.341837, -76.530986] },
+  { name: "JAVERIANA", coords: [3.348674, -76.530412] },
+  { name: "CIUDAD JARDIN", coords: [3.360371, -76.530677] },
+  { name: "CIUDAD JARDIN", coords: [3.365331, -76.530961] },
+  { name: "JARDIN PLAZA", coords: [3.368843, -76.529505] },
+  { name: "ENTRADA MOTOS UNIVALLE", coords: [3.370258,-76.534817] },
+  { name: "UNIVALLE", coords: [3.375138, -76.537198] },
+  { name: "CALLE 13 CON KR 80", coords: [3.386670, -76.538490] },
+  { name: "CALLE 13 CON KR 68 Y 66", coords: [3.397186, -76.537868] },
+  { name: "CRA 56", coords: [3.404232, -76.532812] },
+  { name: "CALLE 14 ENTRE 53 Y 50", coords: [3.406804, -76.530407] },
+  { name: "CALLE 14 ENTRE 43 Y 42", coords: [3.415157, -76.528102] },
+  { name: "CALLE 14 ENTRE 34 Y 33A", coords: [3.422966, -76.527446] },
+  { name: "CALLE 13 CON CL 14", coords: [3.429512, -76.527287] },
+  { name: "CALLE 13", coords: [3.432875, -76.527889] },
+  { name: "CALLE 13", coords: [3.440018, -76.528438] },
+  { name: "CRA.15", coords: [3.442749, -76.529578] },
+  { name: "CALLE 11", coords: [3.444646, -76.530774] },
+  { name: "CRA 10", coords: [3.447563, -76.530360] },
+  { name: "CRA 10 CON CL 21", coords: [3.449236, -76.522661] },
+  { name: "CALLE 10A", coords: [3.448931, -76.522583] },
+  { name: "CALLE 20 #10A", coords: [3.448731, -76.523256] },
+  { name: "CALLE 20 ", coords: [3.450593, -76.523851] },
+  { name: "CRA 8", coords: [3.451278, -76.524713] },
+  { name: "CALLE 18", coords: [3.451990, -76.526220] },
+  { name: "CALLE 18", coords: [3.453409, -76.526826] },
+  { name: "CRA 5", coords: [3.454568, -76.525712] },
+  { name: "CRA 5", coords: [3.456781, -76.520733] },
+  { name: "CRA 5", coords: [3.458531, -76.514322] },
+  { name: "CRA 5 ENTRE CL 37 Y 38", coords: [3.459719, -76.510576] },
+  { name: "CRA 5 ENTRE CL 44 Y 44B", coords: [3.460891, -76.506806] },
+  { name: "CALLE 45", coords: [3.462490, -76.505766] },
+  { name: "CALLE 45 CON KR 2B", coords: [3.465166, -76.506937] },
+  { name: "CRA 2DA", coords: [3.467101, -76.505106] },
+  { name: "CRA 2DA CON CL 52 Y 56", coords: [3.468522, -76.500620] },
+  { name: "SENA", coords: [] },
+  { name: "CALLE 62", coords: [] },
+  { name: "CRA 1D", coords: [] },
+  { name: "OLIMPICA", coords: [] },
+  { name: "CHIMINANGOS", coords: [] },
+  { name: "CALLE 70", coords: [] },
+  { name: "LOS ALCAZARES", coords: [] },
+  { name: "TERMINALITO", coords: [] },
+  { name: "14 DE CALIMA", coords: [] },
+  { name: "SAMECO", coords: [] },
+  { name: "BRISAS DE LOS ALAMOS", coords: [] }
+];
+
+// Añadir los puntos al mapa
+points.forEach(function(point) {
+  L.marker(point.coords)
+      .addTo(map)
+      .bindPopup(point.name);
+});
