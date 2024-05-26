d4622f305c5535e4f15d230a4757a0b11460161a
Allan77-debug <allan.paredes@correounivalle.edu.co>
2024-05-26 17:41:17 -0500

    .gitignore

--- db.php
+++ db.php
@@ -1,9 +1,14 @@
 <?php
-$host = "localhost";
-$port = '5432';
-$dbname = "buses_db";
-$user = "postgres";
-$pass = "bdadmin";
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$dbname = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass");
 
