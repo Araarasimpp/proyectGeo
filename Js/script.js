1b8f0ee5778ba1d353bcfa29d4d39c50b3d26f16
Araarasimpp <nicolasrios.078@gmail.com>
2024-06-03 16:28:59 -0500

    cords
    funciona linea y puntos de ref

--- Js/script.js
+++ Js/script.js
@@ -242,71 +242,117 @@ var routeCoordinates = [
     [3.371330, -76.536908],
     [3.371595, -76.537094],
     [3.375138, -76.537198],
-    
-
-
-
+    [3.378030, -76.537321],
+    [3.381179, -76.537808],
+    [3.382791, -76.538060],
+    [3.385142, -76.538404],
     [3.38667, -76.53849],
+    [3.387837, -76.538538],
+    [3.389249, -76.538485],
+    [3.390140, -76.538437],
     [3.397186, -76.537868],
+    [3.398244, -76.537777],
+    [3.401995, -76.536545],
+    [3.404848, -76.535597],
+    [3.405051, -76.535305],
     [3.404232, -76.532812],
+    [3.403718, -76.531238],
     [3.406804, -76.530407],
     [3.415157, -76.528102],
+    [3.418221, -76.527234],
     [3.422966, -76.527446],
-    [3.429512, -76.527287],
+    [3.428693, -76.527746],
+    [3.428939, -76.527699],
+    [3.429521, -76.527323],
+    [3.430234, -76.526859],
+    [3.430642, -76.526802],
+    [3.430938, -76.526845],
+    [3.431367, -76.527066],
+    [3.432496, -76.527816],
     [3.432875, -76.527889],
+    [3.438952, -76.528242],
     [3.440018, -76.528438],
+    [3.442766, -76.528622],
     [3.442749, -76.529578],
+    [3.442704, -76.530806],
+    [3.444195, -76.530710],
     [3.444646, -76.530774],
+    [3.447222, -76.531502],
     [3.447563, -76.53036],
+    [3.447692, -76.529922],
+    [3.447883, -76.527597],
     [3.449236, -76.522661],
     [3.448931, -76.522583],
     [3.448731, -76.523256],
     [3.450593, -76.523851],
+    [3.451469, -76.524134],
     [3.451278, -76.524713],
-    [3.45199, -76.52622],
-    [3.453409, -76.526826],
+    [3.450906, -76.525763],
+    [3.451988, -76.526232],
+    [3.453414, -76.526841],
+    [3.453971, -76.527044],
     [3.454568, -76.525712],
-    [3.456781, -76.520733],
+    [3.456804, -76.520739],
+    [3.457366, -76.518618],
+    [3.458136, -76.515426],
     [3.458531, -76.514322],
-    [3.459719, -76.510576],
+    [3.459707, -76.510569],
     [3.460891, -76.506806],
+    [3.461364, -76.505297],
     [3.46249, -76.505766],
     [3.465166, -76.506937],
+    [3.466164, -76.507376],
+    [3.466536, -76.506713],
     [3.467101, -76.505106],
+    [3.468013, -76.502216],
+    [3.467896, -76.502012],
+    [3.467995, -76.501779],
+    [3.468185, -76.501717],
     [3.468522, -76.50062],
     [3.470254, -76.494992],
+    [3.471116, -76.492174],
     [3.472629, -76.492630],
+    [3.474578, -76.493203],
     [3.475364, -76.490658],
+    [3.476155, -76.488137],
     [3.475508, -76.487948],
+    [3.474801, -76.487540],
+    [3.474710, -76.487304],
+    [3.474967, -76.487122],
     [3.475306, -76.487207],
     [3.478986, -76.488364],
     [3.481276, -76.489079],
     [3.483202, -76.489726],
+    [3.483914, -76.490214],
+    [3.484551, -76.490957],
     [3.485031, -76.492010],
     [3.486550, -76.496761],
+    [3.487334, -76.498740],
     [3.487591, -76.498654],
+    [3.488726, -76.498362],
+    [3.489046, -76.498346],
     [3.489537, -76.497975],
     [3.491429, -76.496679],
+    [3.494061, -76.494959],
     [3.495221, -76.494327],
     [3.498172, -76.492653],
-    [3.498803, -76.491810],
+    [3.499039, -76.492140],
+    [3.498813, -76.491803],
     [3.496765, -76.487857],
-    [3.495906, -76.487152],
+    [3.496231, -76.486858],
+    [3.495977, -76.487118],
+    [3.495685, -76.487558],
     [3.495454, -76.487525],
     [3.494468, -76.487380],
+    [3.493991, -76.487311],
     [3.494054, -76.486942],
-    [3.493521, -76.486372],
-    [3.492871, -76.486270],
+    [3.494124, -76.486504],
+    [3.493628, -76.486417],
+    [3.492987, -76.486323],
+    [3.492465, -76.486216],
     [3.492536, -76.485826],
     [3.492779, -76.484902]
-
 ];
-
-// Trazar la ruta en el mapa
-var polyline = L.polyline(routeCoordinates, { color: 'blue' }).addTo(map);
-
-// Ajustar la vista del mapa para que incluya la ruta completa
-map.fitBounds(polyline.getBounds());
 
 // Lista de puntos de referencia con sus coordenadas
 var routes = {
@@ -316,71 +362,37 @@ var routes = {
     { name: "ICESI", coords: [3.341837, -76.530986] },
     { name: "JAVERIANA", coords: [3.348674, -76.530412] },
     { name: "CIUDAD JARDIN", coords: [3.360371, -76.530677] },
-    { name: "CIUDAD JARDIN", coords: [3.365331, -76.530961] },
     { name: "JARDIN PLAZA", coords: [3.368843, -76.529505] },
     { name: "ENTRADA MOTOS UNIVALLE", coords: [3.370258, -76.534817] },
     { name: "UNIVALLE", coords: [3.375138, -76.537198] },
-    { name: "CALLE 13 CON KR 80", coords: [3.38667, -76.53849] },
     { name: "CALLE 13 CON KR 68 Y 66", coords: [3.397186, -76.537868] },
     { name: "CRA 56", coords: [3.404232, -76.532812] },
-    { name: "CALLE 14 ENTRE 53 Y 50", coords: [3.406804, -76.530407] },
-    { name: "CALLE 14 ENTRE 43 Y 42", coords: [3.415157, -76.528102] },
     { name: "CALLE 14 ENTRE 34 Y 33A", coords: [3.422966, -76.527446] },
-    { name: "CALLE 13 CON CL 14", coords: [3.429512, -76.527287] },
-    { name: "CALLE 13", coords: [3.432875, -76.527889] },
     { name: "CALLE 13", coords: [3.440018, -76.528438] },
-    { name: "CRA.15", coords: [3.442749, -76.529578] },
-    { name: "CALLE 11", coords: [3.444646, -76.530774] },
-    { name: "CRA 10", coords: [3.447563, -76.53036] },
-    { name: "CRA 10 CON CL 21", coords: [3.449236, -76.522661] },
-    { name: "CALLE 10A", coords: [3.448931, -76.522583] },
-    { name: "CALLE 20 #10A", coords: [3.448731, -76.523256] },
     { name: "CALLE 20 ", coords: [3.450593, -76.523851] },
-    { name: "CRA 8", coords: [3.451278, -76.524713] },
-    { name: "CALLE 18", coords: [3.45199, -76.52622] },
-    { name: "CALLE 18", coords: [3.453409, -76.526826] },
-    { name: "CRA 5", coords: [3.454568, -76.525712] },
     { name: "CRA 5", coords: [3.456781, -76.520733] },
-    { name: "CRA 5", coords: [3.458531, -76.514322] },
-    { name: "CRA 5 ENTRE CL 37 Y 38", coords: [3.459719, -76.510576] },
     { name: "CRA 5 ENTRE CL 44 Y 44B", coords: [3.460891, -76.506806] },
-    { name: "CALLE 45", coords: [3.46249, -76.505766] },
-    { name: "CALLE 45 CON KR 2B", coords: [3.465166, -76.506937] },
-    { name: "CRA 2DA", coords: [3.467101, -76.505106] },
-    { name: "CRA 2DA CON CL 52 Y 56", coords: [3.468522, -76.50062] },
     { name: "CRA 2DA CON CL 59B Y 59C", coords: [3.470254, -76.494992] },
-    { name: "CL 62 ENTRE Kr 1HBIS Y 1H", coords: [3.472629, -76.492630] },
-    { name: "CRA 1D", coords: [3.475364, -76.490658] },
-    { name: "CL 70", coords: [3.475508, -76.487948] },
-    { name: "CL 70 ENTRE KR 1GBIS Y 1F", coords: [3.475306, -76.487207] },
     { name: "AUTO ORIENTAL", coords: [3.478986, -76.488364] },
-    { name: "AUTO ORIENTAL", coords: [3.481276, -76.489079] },
-    { name: "AUTO ORIENTAL", coords: [3.483202, -76.489726] },
-    { name: "AUTO ORIENTAL", coords: [3.485031, -76.492010] },
-    { name: "AUTO ORIENTAL", coords: [3.486550, -76.496761] },
-    { name: "KR 4AN ENTRE CL 70 Y 71", coords: [3.487591, -76.498654] },
     { name: "KR 4AN", coords: [3.489537, -76.497975] },
-    { name: "KR 4N ENTRE CL 72 Y 72BIS", coords: [3.491429, -76.496679] },
-    { name: "KR 4N ENTRE CL 72K Y 72J", coords: [3.495221, -76.494327] },
-    { name: "KR 4N ENTRE CL 82 Y 83", coords: [3.498172, -76.492653] },
     { name: "CL 84", coords: [3.498803, -76.491810] },
     { name: "CL 84 ENTRE KR 2NBIS Y 2AN", coords: [3.496765, -76.487857] },
-    { name: "DIAGONAL 7NTE", coords: [3.495906, -76.487152] },
-    { name: "CL 81NTE", coords: [3.495454, -76.487525] },
-    { name: "CL 81NTE", coords: [3.494468, -76.487380] },
-    { name: "KR 1FN ENTRE CL 81 Y 80", coords: [3.494054, -76.486942] },
     { name: "CL 82NTE", coords: [3.493521, -76.486372] },
-    { name: "CL 82NTE", coords: [3.492871, -76.486270] },
-    { name: "CRA 1NTE", coords: [3.492536, -76.485826] },
-    { name: "CRA 1NTE", coords: [3.492779, -76.484902] },
   ],
 };
 
+// Crear ícono personalizado
+var customIcon = L.icon({
+  iconUrl: 'src/ruta.png', // Asegúrate de que la ruta a la imagen sea correcta
+  iconSize: [80, 80], // Tamaño del ícono
+  popupAnchor: [1, -34] // Punto desde el cual se abrirá el popup relativo al icono
+});
+
+// Función para mostrar la ruta y los puntos de referencia
 function showRoute() {
   var routeInput = document.getElementById('routeInput').value;
   var routeId = parseInt(routeInput);
 
-  // Verifica si la ruta existe
   if (routes[routeId]) {
     // Limpia las capas existentes en el mapa
     map.eachLayer(function (layer) {
@@ -391,18 +403,23 @@ function showRoute() {
 
     // Añadir capa de mapa base nuevamente
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // Añadir la línea de la ruta al mapa con estilo discontínuo
+    var polyline = L.polyline(routeCoordinates, {
+      color: 'red',
+      dashArray: '5, 10', // Patrón de guiones (5 píxeles de línea, 10 píxeles de espacio)
+      weight: 2 //Grosor de la linea
     }).addTo(map);
 
     // Añadir puntos de referencia al mapa
     routes[routeId].forEach(function (point) {
-      L.marker(point.coords).addTo(map).bindPopup(point.name);
+      L.marker(point.coords, { icon: customIcon }).addTo(map).bindPopup(point.name);
     });
 
-    // Ajustar la vista del mapa para incluir todos los puntos de referencia
-    var bounds = routes[routeId].map(function (point) {
-      return point.coords;
-    });
+    // Ajustar la vista del mapa para incluir la ruta completa y los puntos de referencia
+    var bounds = L.latLngBounds(routeCoordinates.concat(routes[routeId].map(point => point.coords)));
     map.fitBounds(bounds);
   } else {
     alert('Ruta no encontrada');
